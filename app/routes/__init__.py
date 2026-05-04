b3113cb7ab8da6d444c7dc7eb9f490a882347b86
Prasetya <prasetya.career@gmail.com>
2026-05-04 12:50:52 +0700

    m

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,7 +3,10 @@ import traceback
 def register_routes(app):
     try:
         from .app_routes import main_bp
+        from .monitor_routes import monitor_bp
+
         app.register_blueprint(main_bp)
+        app.register_blueprint(monitor_bp)
 
     except Exception as e:
         print("\nROUTE gagal di regis dan inisialisasi di create_app()")
